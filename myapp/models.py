d8addeb092b2601b973c2fffa094ebfe2cb7b896
Himanshu <kandpallucky@gmail.com>
2024-02-23 18:02:43 +0530

    created a name page

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Profiles - creates object containing
+# user profile details
+class Profile(models.Model):
+    name = models.CharField(max_length=255)
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render, redirect
+from myapp.models import Profile 
 
-# Create your views here.
+# Create your views here
 def welcome(request):
     return HttpResponse("Customise welcome page.")
 
 def hello_world(request):
     return HttpResponse("Welcome to the exciting world of Django!")
+
+def profile_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        Profile.objects.create(name=name)
+        return redirect('profile')
+    else:
+        stored_names = Profile.objects.all()
+        context = {'stored_names': stored_names}
+        return render(request, 'profile.html', context)
