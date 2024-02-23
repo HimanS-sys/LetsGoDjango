862c3b7485f58ba4719542eeabe76090f2d67c9a
Himanshu <kandpallucky@gmail.com>
2024-02-23 16:31:27 +0530

    added hello_world module

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse("Customise welcome page.")
+
+def hello_world(request):
+    return HttpResponse("Welcome to the exciting world of Django!")
